0c612a6914e23ec11f0c5c5cf7a97c064655fecf
xhj <872621336@qq.com>
2018-04-30 10:36:40 +0800

    Demo-GitHub-v1.0

---
+++ src/main/java/com/xhj/gitdemo/pojo/AdminMan.java
@@ -0,0 +1,43 @@
+package com.xhj.gitdemo.pojo;
+
+/**
+ * Created by xhj on 2018/4/30/030.
+ */
+public class AdminMan {
+    private Integer aid;
+    private String aname;
+    private String apwd;
+    private String pflag;
+
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getApwd() {
+        return apwd;
+    }
+
+    public void setApwd(String apwd) {
+        this.apwd = apwd;
+    }
+
+    public String getPflag() {
+        return pflag;
+    }
+
+    public void setPflag(String pflag) {
+        this.pflag = pflag;
+    }
+}
---
+++ src/main/java/com/xhj/gitdemo/pojo/User.java
@@ -0,0 +1,43 @@
+package com.xhj.gitdemo.pojo;
+
+/**
+ * Created by xhj on 2018/4/30/030.
+ */
+public class User {
+    private Integer uid;
+    private String uname;
+    private String upwd;
+    private String pflag;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpwd() {
+        return upwd;
+    }
+
+    public void setUpwd(String upwd) {
+        this.upwd = upwd;
+    }
+
+    public String getPflag() {
+        return pflag;
+    }
+
+    public void setPflag(String pflag) {
+        this.pflag = pflag;
+    }
+}
